9f14787685f980ba16ab77563b8bcd6a765dff24
Danilo <damilhome@gmail.com>
2024-11-05 16:12:36 -0300

    questao 07 feita

---
+++ Programação com Python/Teste de Performance 2/questao_07.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que defina a lista:
+[10, 20, 30, 40, 50, 60, 70, 80]
+e mostre seu conteúdo na ordem invertida.
+
+Utilize apenas os comandos ensinados em aula.
+'''
+
+def invert_list(list):
+    '''Returns the list reversed'''
+    return list[::-1]
+
+print(invert_list([10, 20, 30, 40, 50, 60, 70, 80]))
