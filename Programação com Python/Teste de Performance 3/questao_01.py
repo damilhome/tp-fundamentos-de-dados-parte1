2c09c43503d18ad6ec7f2be8c443f4a272f38434
Danilo <damilhome@gmail.com>
2024-11-21 16:16:42 -0300

    questao 1 tp3 feita

---
+++ Programação com Python/Teste de Performance 3/questao_01.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um nome e sobrenome e exiba-os na mesma linha
+
+nome = input('Entre com o nome: ')
+sobrenome = input('Entre com o sobrenome: ')
+print(nome + ' ' + sobrenome)
