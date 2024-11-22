769d61ff6e4bb3c9c25507a80a4070b72cc0172c
Danilo <damilhome@gmail.com>
2024-11-22 16:27:14 -0300

    questao 13 tp3 feita

---
+++ Programação com Python/Teste de Performance 3/questao_13.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que leia a frase abaixo e mostre a frase invertida, utilizando o comando [::] de manipulação de strings. 
+'''
+
+FRASE = 'eu gosto de programar'
+
+def mostrar_frase_invertida():
+    print(FRASE[::-1])
+
+mostrar_frase_invertida()
